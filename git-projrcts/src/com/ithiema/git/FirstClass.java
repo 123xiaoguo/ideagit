7ef9949dbbac409f49ed249b4af7309e7d3faea4
guoxiaoli <1564464275@qq.com>
2020-07-01 10:25:23 +0800

    把本地工程添加到git

---
+++ git-projrcts/src/com/ithiema/git/FirstClass.java
@@ -0,0 +1,10 @@
+package com.ithiema.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("aaaaaaa");
+        System.out.println("aaaaaaa");
+        System.out.println("aaaaaaa");
+        System.out.println("aaaaaaa");
+    }
+}
