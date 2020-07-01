963056317f408b85f9c4af2a64dcf808d83e7f63
guoxiaoli <1564464275@qq.com>
2020-07-01 11:38:50 +0800

    修改firstClass

--- git-projrcts/src/com/ithiema/git/FirstClass.java
+++ git-projrcts/src/com/ithiema/git/FirstClass.java
@@ -6,6 +6,7 @@ public class FirstClass {
         System.out.println("aaaaaaa");
         System.out.println("aaaaaaa");
         System.out.println("aaaaaaa");
+        //hello
     }
 
     public void test(){
